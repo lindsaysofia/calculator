cda3cb58c4b5ae689985f24220e6a58fe1da19bc
Lindsay Lopez <lopez95@berkeley.edu>
2021-05-22 22:08:37 -0700

    added HTML for display and buttons. Now need to style them

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const buttonsContainer = document.querySelector('.buttons-container');
+
 const operations = {
   add: function (firstNumber, secondNumber) {
     return firstNumber + secondNumber;
@@ -12,6 +14,26 @@ const operations = {
     return firstNumber / secondNumber;
   }
 };
+
+const buttons = [
+  {id: 7, display: 7}, {id: 8, display: 8}, {id: 9, display: 9}, {id: 'divide', display: '/'},
+  {id: 4, display: 4}, {id: 5, display: 5}, {id: 6, display: 6}, {id: 'multiply', display: '*'},
+  {id: 1, display: 1}, {id: 2, display: 2}, {id: 3, display: 3}, {id: 'subtract', display: '-'},
+  {id: 0, display: 0}, {id: 'decimal', display: '.'}, {id: 'equals', display: '='}, {id: 'add', display: '+'},
+  {id: 'clear', display: 'CLEAR'}
+];
+
+buttons.forEach(button => {
+  let newButtonElement = document.createElement('button');
+  newButtonElement.id = button.id;
+  newButtonElement.textContent = button.display;
+  newButtonElement.addEventListener('click', handleClick);
+  buttonsContainer.appendChild(newButtonElement);
+});
+
+function handleClick(e) {
+  console.log(typeof e.target.id);
+}
 
 function operate(operator, firstNumber, secondNumber) {
   return operations[operator](firstNumber, secondNumber);
